6bc20f5100c02d1e31671866af6fd2054685b0d1
zhz <1871229316@qq.com>
2019-12-16 23:35:59 +0800

    SpringBoot 中异常处理与单元测试-通过@ExceptionHandler 注解处理异常

--- springboot-excepionandjunit/src/main/java/com/hr/springbootexcepionandjunit/controller/UserController.java
+++ springboot-excepionandjunit/src/main/java/com/hr/springbootexcepionandjunit/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.hr.springbootexcepionandjunit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author 周恒哲
@@ -15,5 +17,12 @@ public class UserController {
         String str = null;
         str.length();
         return "ok";
+    }
+    @ExceptionHandler(value = java.lang.NullPointerException.class)
+    public ModelAndView nullpointExceptionHandler(Exception e){
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("err",e.toString());
+        mv.setViewName("error1");
+        return mv;
     }
 }
