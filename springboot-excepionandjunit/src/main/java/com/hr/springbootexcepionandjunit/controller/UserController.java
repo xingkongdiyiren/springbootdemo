fa045f5f60474c37d252ef61a36f4a176b540c17
zhz <1871229316@qq.com>
2019-12-16 23:20:29 +0800

    SpringBoot 中异常处理与单元测试-自定义错误页面

---
+++ springboot-excepionandjunit/src/main/java/com/hr/springbootexcepionandjunit/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.hr.springbootexcepionandjunit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 周恒哲
+ * @date 2019/12/16
+ **/
+@Controller
+public class UserController {
+
+    @RequestMapping
+    public String showInfo(){
+        String str = null;
+        str.length();
+        return "ok";
+    }
+}
